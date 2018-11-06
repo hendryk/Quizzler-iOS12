14df71b2e5a9cff567523ebaaaecd62df062da5e
krzyzak <krzyzakov@gmail.com>
2018-11-06 23:53:23 +0100

    Już działa

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestinBank()
+    var pickedAnswer: Bool = false
+    var questionNumber: Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,12 +21,24 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextQuestion()
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }
+        else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        
+        checkAnswer()
+        
+        questionNumber = questionNumber + 1
+        
+        nextQuestion()
     }
     
     
@@ -35,12 +48,25 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        if questionNumber <= 12 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+            print("Koniec konkursu")
+            questionNumber = 0
+        }
     }
     
     
     func checkAnswer() {
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
+        if correctAnswer == pickedAnswer {
+            print("Bardzo dobrze!")
+        }
+        else {
+            print("Źle!!!")
+        }
     }
     
     
