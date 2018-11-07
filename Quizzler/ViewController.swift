8c6283bad4cfe37e1fd5c57fdf9d6d43561e8323
Jacek Krzyżanowski <krzyzakov@gmail.com>
2018-11-07 09:44:23 +0100

    Komunikat końca pytań

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     let allQuestions = QuestinBank()
     var pickedAnswer: Bool = false
     var questionNumber: Int = 0
-    
+
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     @IBOutlet var progressBar: UIView!
     @IBOutlet weak var progressLabel: UILabel!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         nextQuestion()
@@ -32,35 +32,46 @@ class ViewController: UIViewController {
         else if sender.tag == 2 {
             pickedAnswer = false
         }
-        
-        
+
+
         checkAnswer()
-        
+
         questionNumber = questionNumber + 1
-        
+
         nextQuestion()
     }
-    
-    
+
+
     func updateUI() {
-      
+
     }
-    
+
 
     func nextQuestion() {
+
         if questionNumber <= 12 {
             questionLabel.text = allQuestions.list[questionNumber].questionText
         }
         else {
             print("Koniec konkursu")
-            questionNumber = 0
+
+            let alert = UIAlertController(title: "Super", message: "Wszystkie pytania przerobione. Jeszcze raz?", preferredStyle: .alert)
+
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { (UIAlertAction) in
+                self.startOver()
+            }
+
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+
         }
     }
-    
-    
+
+
     func checkAnswer() {
+
         let correctAnswer = allQuestions.list[questionNumber].answer
-        
+
         if correctAnswer == pickedAnswer {
             print("Bardzo dobrze!")
         }
@@ -68,12 +79,13 @@ class ViewController: UIViewController {
             print("Źle!!!")
         }
     }
-    
-    
-    func startOver() {
-       
-    }
-    
 
-    
+
+    func startOver() {
+        questionNumber = 0
+        nextQuestion()
+    }
+
+
+
 }
